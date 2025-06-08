f01e4e3aa8ee1aaa1ff89367f4bef7bc73e1aec1
Alexander D.M. Broom <alex.broom@gmail.com>
2025-06-08 00:42:07 -0400

    Dice layout (#3)
    * dice layout updated
    
    * Improved menu dialogs
    
    * active button styling
    
    * remove unused typo style

--- src/components/CrawlerSheetPage/Menu/DiceRoller/DiceRoller.tsx
+++ src/components/CrawlerSheetPage/Menu/DiceRoller/DiceRoller.tsx
@@ -4,8 +4,14 @@ import clsx from 'clsx/lite';
 
 import { shuffleArray } from '@/utils/ShuffleArray';
 
-export const DiceRoller = ({ close }: { readonly close: () => void }) => {
-	const [customDice, setCustomDice] = useState<number>(6);
+export const DiceRoller = ({
+	visible,
+	close,
+}: {
+	readonly visible: boolean;
+	readonly close: () => void;
+}) => {
+	const [customDice, setCustomDice] = useState<number>(7);
 	const [diceRolls, setDiceRolls] = useState<
 		({
 			readonly timestamp: Date;
@@ -41,40 +47,13 @@ export const DiceRoller = ({ close }: { readonly close: () => void }) => {
 	});
 
 	return (
-		<div className="fixed inset-0 flex flex-col gap-4 sm:gap-8 p-4 sm:p-8 bg-(--color-primary)">
+		<div
+			className={clsx(
+				visible ? 'fixed' : 'hidden',
+				'inset-0 flex flex-col gap-4 sm:gap-8 p-4 sm:p-8 bg-(--color-primary)',
+			)}
+		>
 			<div className="flex-grow grid grid-cols-1 sm:grid-cols-2 gap-2 overflow-hidden">
-				<div className="order-last sm:order-first border border-(--color-primary-3) rounded-lg flex-grow max-h-50% p-2 grid grid-cols-2 sm:gris-cols-1 gap-2 overflow-auto overscroll-contain">
-					{[20, 2, 3, 4, 6, 8, 10, 12, 100].map((sides) => (
-						<button
-							key={sides}
-							onClick={() => handleRoll(sides)}
-							className={clsx(
-								'w-full',
-								sides === 20 && 'col-span-full text-3xl',
-							)}
-						>
-							Roll d{sides}
-						</button>
-					))}
-					<div className="col-span-full flex items-center gap-2">
-						<label className="text-lg" htmlFor="custom-dice">
-							Custom
-						</label>
-						<input
-							id="custom-dice"
-							type="number"
-							className="w-20 text-center"
-							value={customDice}
-							onChange={(e) => setCustomDice(Number(e.target.value) || 6)}
-						/>
-						<button
-							className="flex-grow"
-							onClick={() => handleRoll(customDice)}
-						>
-							Roll d{customDice}
-						</button>
-					</div>
-				</div>
 				<div className="border border-(--color-primary-2) rounded-lg flex-grow max-h-50% p-4 bg-(--color-primary-2) overflow-auto overscroll-contain">
 					<label>Dice Results</label>
 					<div className="flex flex-col gap-2">
@@ -108,6 +87,37 @@ export const DiceRoller = ({ close }: { readonly close: () => void }) => {
 						)}
 					</div>
 					<div ref={scrollToRef} />
+				</div>
+				<div className="border border-(--color-primary-3) rounded-lg flex-grow max-h-50% p-2 grid grid-cols-2 gap-2 overflow-auto overscroll-contain">
+					{[20, 2, 3, 4, 6, 8, 10, 12, 100].map((sides) => (
+						<button
+							key={sides}
+							onClick={() => handleRoll(sides)}
+							className={clsx(
+								'w-full',
+								sides === 20 && 'col-span-full text-3xl',
+							)}
+						>
+							Roll d{sides}
+						</button>
+					))}
+					<div className="col-span-full grid grid-cols-2 items-center gap-2">
+						<div className="h-full flex flex-col">
+							<label className="hidden sm:block text-xl" htmlFor="custom-dice">
+								Custom
+							</label>
+							<input
+								id="custom-dice"
+								type="number"
+								className="flex-grow h-0 w-full text-center sm:text-xl"
+								value={customDice}
+								onChange={(e) => setCustomDice(Number(e.target.value) || 6)}
+							/>
+						</div>
+						<button className="h-full" onClick={() => handleRoll(customDice)}>
+							Roll d{customDice}
+						</button>
+					</div>
 				</div>
 			</div>
 			<div className="flex justify-center">
